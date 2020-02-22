d60ac948fc0ba276f5d794609585a6001dad0f6c
Max T <toorm@mail.gvsu.edu>
2020-02-21 20:06:52 -0500

    coding has begun

---
+++ extension/content.js
@@ -0,0 +1,10 @@
+let titleElements = document.querySelectorAll("#video-title");
+
+console.log("beginning");
+console.log(titleElements.length);
+for (let i=0; i<titleElements.length; ++i) {
+	console.log(titleElements[i]);	
+}
+
+console.log("ending");
+
