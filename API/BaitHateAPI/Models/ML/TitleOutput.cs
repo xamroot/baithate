dd9cf935c931a8f9f658a9086da75ab0e2fcd7d7
matthewshan <matthewshan99@gmail.com>
2020-02-21 23:09:00 -0500

    GetPredictions working

---
+++ API/BaitHateAPI/Models/ML/TitleOutput.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.ML.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BaitHateAPI.Models.ML
+{
+    public class TitleOutput
+    {
+        [ColumnName("Score")]
+        public float Results { get; set; } 
+
+        public float Probabilities { get {
+                    float k = (float) Math.Exp(Results);
+                    return k / (1.0f + k);
+            }
+        }
+    }
+}
