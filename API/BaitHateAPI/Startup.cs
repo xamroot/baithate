f044da07f7407f88c74406951b80c0ae709bd3c4
matthewshan <matthewshan99@gmail.com>
2020-02-21 21:49:18 -0500

    Feedback call working

--- API/BaitHateAPI/Startup.cs
+++ API/BaitHateAPI/Startup.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using BaitHateAPI.Models;
+//using BaitHateAPI.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
