939739f3d1fec676c91b1540d0cc4668bf8746a2
Dylan Strohschein <dylanstrohschein@gmail.com>
2020-02-22 04:42:53 -0500

    Ya so youtube does some neat caching of their page... cool

--- extension/content.js
+++ extension/content.js
@@ -1,9 +1,14 @@
-let titles = [];
+let alreadySearched = [];
+let currentTitles = [];
+let elementToAppend;
 let buttons = [];
 let targetUrl = "https://baithateapi.azurewebsites.net/api/BaitHate";
 let currentPopupParent = 0;
 let currentPopup = 0;
 let xhttp = new XMLHttpRequest();
+let canSetInterval = 0
+let readyToLoad = true;
+let loadedUrl = "";
 
 let displayPopup = (event) => {
 	// check to make sure no pop up is running yet
@@ -46,22 +51,39 @@ let displayPopup = (event) => {
 
 }
 
-
 window.onload = function() {
+	console.log("loaded")
+	scrapePage();
+	setInterval(scrapePage, 1000);
+}();
+
+function scrapePage(){
+
+	console.log(window.location.toString());
+	if(loadedUrl !== window.location.toString()){
+		loadedUrl = window.location.toString();
+		alreadySearched = [];
+		currentTitles = [];
+		console.log("has changed");
+		readyToLoad = true;
+		killAll();
+		return;
+	}
+
 	var location = window.location.toString()
-	let titleElements = []
-	let elementToAppend;
-	
+	let titleElements = [];
+	currentTitles = [];
+
 	if (location.includes("youtube.com")) {
 		// init titleElements
-		let titleElements = document.querySelectorAll("#video-title");
+		titleElements = document.querySelectorAll("#video-title");
 		
 		// search results page
 		if (location.includes("/results?")) {
 			elementToAppend = YoutubeSearchpage;
 		// video player page
 		} else if (location.includes("/watch?v=")) {
-			elementToAppend = YoutubeWatchpage;
+			elementToAppend = YoutubeWatchPage;
 		// home page
 		} else {
 			elementToAppend = YoutubeHomepage;
@@ -69,55 +91,74 @@ window.onload = function() {
 	} 
 
 	for (let i=0; i<titleElements.length; ++i) {
-		titles.push(titleElements[i].innerText);
+		currentTitles.push(titleElements[i].innerText);
 	}
-
-	$.ajax({
-		type: "POST",
-		url: "https://baithateapi.azurewebsites.net/api/BaitHate/GetPrediction",
-		data: JSON.stringify(titles),
-		contentType: "application/json",
-		success: function(data){
-			let x = data;
-			// loop through all response json text
-			for (let i=0; i<x.length; ++i) {
-				var buttonElem = document.createElement("BUTTON");
-				var percent = document.createElement("span");
-				
-				elementToAppend(titleElements[i]).appendChild(buttonElem);
-				elementToAppend(titleElements[i]).appendChild(percent);
-
-				buttonElem.addEventListener("click", displayPopup);
-				buttonElem.setAttribute("result", "N/A");
-				buttonElem.setAttribute("id", "buttonElem" + i);
-				buttonElem.setAttribute("style", "border:1px,1px,1px");
-				let val = parseInt(x[i] * 100)
-				percent.innerText = "  " + val + "%";
-				let color = "blue"
-				buttonElem.setAttribute("result", val)
-				console.log(val)
-				color = getColorGradient(x[i]);
-
-				buttonElem.setAttribute("style", "border:None; border-radius:100%; height:2vh; width:2vh; background-color:"+color+";");
+	
+	updateDifferences(titleElements);
+}
+ 
+function updateDifferences(titleElements){
+	let currentDomOffset = alreadySearched.length;
+	let newItems = currentTitles.slice(alreadySearched.length);
+	alreadySearched = currentTitles;
+	
+	if(newItems.length){
+		$.ajax({
+			type: "POST",
+			url: "https://baithateapi.azurewebsites.net/api/BaitHate/GetPrediction",
+			data: JSON.stringify(newItems),
+			contentType: "application/json",
+			success: function(data){
+				let x = data;
+				console.log(data);
+				// loop through all response json text
+				for (let i=0; i<x.length; ++i) {
+					var buttonElem = document.createElement("BUTTON");
+					var percent = document.createElement("span");
+					percent.setAttribute("id", "spanItemPercentElem" + i);
+					elementToAppend(titleElements[currentDomOffset + i]).appendChild(buttonElem);
+					elementToAppend(titleElements[currentDomOffset + i]).appendChild(percent);
+	
+					buttonElem.addEventListener("click", displayPopup);
+					buttonElem.setAttribute("result", "N/A");
+					buttonElem.setAttribute("id", "buttonElem" + i);
+					buttonElem.setAttribute("style", "border:1px,1px,1px;");
+					let val = parseInt(x[i] * 100)
+					percent.innerText = "  " + val + "%";
+					let color = "blue"
+					buttonElem.setAttribute("result", val)
+					color = getColorGradient(x[i]);
+	
+					buttonElem.setAttribute("style", "border:None; border-radius:100%; height:2vh; width:2vh; background-color:"+color+";display:inline-block;");
+					
+				}
+				readyToLoad = true;
+			},
+			error: function(){
+				readyToLoad = true;
 			}
-		}
-	});
-}();
+		});	
+	}
+}
 
-let getColorGradient = (value) => {
+function getColorGradient(value) {
     var hue=((1-value)*120).toString(10);
-    return ["hsl(",hue,",100%,50%)"].join("");
+	return ["hsl(",hue,",100%,50%)"].join("");
 }
 
-
-let YoutubeHomepage = (titleElement) => {
+function YoutubeHomepage(titleElement) {
 	return titleElement.parentElement.parentElement;
 }
 
-let YoutubeSearchpage = (titleElement) => {
-	return titleElement.parentElement.parentElement;
+function YoutubeSearchpage (titleElement) {
+	return titleElement.parentElement.parentElement.parentElement;
 }
 
-let YoutubeWatchPage = (titleElement) => {
-	return titleElement.parentElement.parentElement;
+function YoutubeWatchPage(titleElement) {
+	return titleElement.parentElement.parentElement.parentElement.parentElement;
+}
+
+function killAll(){
+	$('[id*="buttonElem"]').remove();
+	$('[id*="spanItemPercentElem"]').remove();
 }
